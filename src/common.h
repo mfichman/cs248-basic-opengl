5090baa9276378e5680e5194149e45a4902bc637
Matt Fichman <matt.fichman@gmail.com>
2011-01-07 14:27:21 -0800

    Added build

--- src/common.h
+++ src/common.h
@@ -3,19 +3,22 @@
  * CS 248                                                                   *
  ****************************************************************************/
 
+#include "config.h"
+
 #include <SDL/SDL.h>
 
 #ifdef WINDOWS
 /* On Windows we need windows.h */
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <GL/gl.h>
 #endif
 
-#ifdef OSX 
+#ifdef HAVE_OPENGL_GL_H 
 /* OS X has to be different for some reason */
 #include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#else
+#endif
+
+#ifdef HAVE_GL_GL_H
 #include <GL/gl.h>
-#include <GL/glu.h>
 #endif
