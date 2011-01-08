a1bb68929ab75c294123633808601f75943fd7f2
Matt Fichman <matt.fichman@gmail.com>
2011-01-08 13:57:24 -0800

    Fixed VS

--- common.h
+++ common.h
@@ -8,19 +8,21 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <GL/gl.h>
-#include <SDL/SDL_main.h>
+#include <SDL_main.h>
+#include <SDL.h>
 #endif
 /* OS X has to be different for some reason */
 
 #ifdef __APPLE__
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
+#include <SDL/SDL.h>
 #endif
 
 #ifdef __linux__
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <SDL/SDL.h>
 #endif
 
-#include <SDL/SDL.h>
 
